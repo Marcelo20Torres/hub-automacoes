0eca55078d03d4d1782673508411fd0059b767da
Marcelo Torres <torresmarcelo316@gmail.com>
2025-04-11 22:08:48 -0300

    Adicionando servidor Flask com automaçao exemplo

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import importlib.util
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Servidor de Automação Online"
+
+@app.route('/executar/<nome_automacao>', methods=['POST'])
+def executar_automacao(nome_automacao):
+    caminho = f'automacoes/{nome_automacao}.py'
+
+    if not os.path.exists(caminho):
+        return jsonify({"erro": "Automação não encontrada"}), 404
+    
+    spec = importlib.util.spec_from_file_location("modulo_automacao", caminho)
+    modulo = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(modulo)
+
+    args = request.json.get("args", [])
+    resultado = modulo.executar(args)
+
+    return jsonify({"resultado": resultado})
+
+if __name__ == '__main__': 
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
---
+++ automacoes/relatorio.py
@@ -0,0 +1,3 @@
+def executar(args=None):
+    nome = args[0] if args else "Mundo"
+    return f"   Relatorio gerado com sucesso para {nome}"
